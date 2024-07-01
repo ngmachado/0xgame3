e2e7fe0fbe5fc553403b21d8ea7d7e261bca6a49
Axe <seamac@gmail.com>
2024-07-01 22:21:50 +0100

    polish camera

--- zig3D/src/camera.zig
+++ zig3D/src/camera.zig
@@ -6,7 +6,7 @@ const utils = @import("utils.zig");
 
 pub const Camera = struct {
     position: vector.Vec3f,
-    target_position: vector.Vec3f,
+    orientation: vector.Vec3f,
     screen_width: usize,
     screen_height: usize,
     fov_factor: f32,
@@ -14,70 +14,88 @@ pub const Camera = struct {
     far_plane: f32,
     speed: f32,
 
-    pub fn init(position: vector.Vec3f, screen_width: usize, screen_height: usize, fov: f32, near_plane: f32, far_plane: f32, movement_speed: f32) Camera {
+    pub const Config = struct {
+        position: vector.Vec3f,
+        orientation: vector.Vec3f,
+        screen_width: usize,
+        screen_height: usize,
+        fov_factor: f32,
+        near_plane: f32,
+        far_plane: f32,
+        speed: f32,
+    };
+
+    pub fn init(config: Config) Camera {
         return Camera{
-            .position = position,
-            .target_position = vector.Vec3f.init(0.0, 0.0, 0.0),
-            .screen_width = screen_width,
-            .screen_height = screen_height,
-            .fov_factor = fov,
-            .near_plane = near_plane,
-            .far_plane = far_plane,
-            .speed = movement_speed,
+            .position = config.position,
+            .orientation = config.orientation,
+            .screen_width = config.screen_width,
+            .screen_height = config.screen_height,
+            .fov_factor = config.fov_factor,
+            .near_plane = config.near_plane,
+            .far_plane = config.far_plane,
+            .speed = config.speed,
         };
     }
 
     pub fn getScreenWidth(self: *Camera) f32 {
-        return @floatFromInt(self.screen_width);
+        return utils.castUsizeToF32(self.screen_width);
     }
 
     pub fn getScreenHeight(self: *Camera) f32 {
-        return @floatFromInt(self.screen_height);
+        return utils.castUsizeToF32(self.screen_height);
     }
 
     pub fn getHalfScreenWidth(self: *Camera) f32 {
-        const a = utils.castU32xF32(self.screen_width);
-        return a / 2.0;
+        return utils.castUsizeToF32(self.screen_width) / 2.0;
     }
 
     pub fn getHalfScreenHeight(self: *Camera) f32 {
-        const a = utils.castU32xF32(self.screen_height);
-        return a / 2.0;
+        return utils.castUsizeToF32(self.screen_height) / 2.0;
     }
 
     pub fn updatePosition(self: *Camera, delta_time: f32) void {
         if (self.target_position.isZero()) {
             return;
         }
-        const target = self.target_position;
-        const movement = target.mulScalar(self.speed).mulScalar(delta_time);
+        const movement = self.target_position.mulScalar(self.speed).mulScalar(delta_time);
         self.position = self.position.add(movement);
-        // reset the target position
-        self.target_position = vector.Vec3f.init(0.0, 0.0, 0.0);
+        self.resetTargetPosition();
     }
 
-    pub fn moveForward(self: *Camera, units: f32) void {
-        self.target_position = vector.Vec3f.init(0.0, 0.0, units);
+    pub fn move(self: *Camera, direction: vector.Vec3f, delta_time: f32) void {
+        const forward = self.getForwardVector().mulScalar(direction.z);
+        const right = self.getRightVector().mulScalar(direction.x);
+        const up = self.getUpVector().mulScalar(direction.y);
+        const left = right.negate();
+        const down = up.negate();
+
+        const movement = forward.add(right).add(up).add(left).add(down).mulScalar(self.speed).mulScalar(delta_time);
+        self.position = self.position.add(movement);
     }
 
-    pub fn moveBackward(self: *Camera, units: f32) void {
-        self.target_position = vector.Vec3f.init(0.0, 0.0, units);
+    pub fn moveForward(self: *Camera, units: f32, delta_time: f32) void {
+        self.move(vector.Vec3f.init(0.0, 0.0, units), delta_time);
     }
 
-    pub fn moveLeft(self: *Camera, units: f32) void {
-        self.target_position = vector.Vec3f.init(units, 0.0, 0.0);
+    pub fn moveBackward(self: *Camera, units: f32, delta_time: f32) void {
+        self.move(vector.Vec3f.init(0.0, 0.0, -units), delta_time);
     }
 
-    pub fn moveRight(self: *Camera, units: f32) void {
-        self.target_position = vector.Vec3f.init(units, 0.0, 0.0);
+    pub fn moveLeft(self: *Camera, units: f32, delta_time: f32) void {
+        self.move(vector.Vec3f.init(-units, 0.0, 0.0), delta_time);
     }
 
-    pub fn moveUp(self: *Camera, units: f32) void {
-        self.target_position = vector.Vec3f.init(0.0, units, 0.0);
+    pub fn moveRight(self: *Camera, units: f32, delta_time: f32) void {
+        self.move(vector.Vec3f.init(units, 0.0, 0.0), delta_time);
     }
 
-    pub fn moveDown(self: *Camera, units: f32) void {
-        self.target_position = vector.Vec3f.init(0.0, units, 0.0);
+    pub fn moveUp(self: *Camera, units: f32, delta_time: f32) void {
+        self.move(vector.Vec3f.init(0.0, units, 0.0), delta_time);
+    }
+
+    pub fn moveDown(self: *Camera, units: f32, delta_time: f32) void {
+        self.move(vector.Vec3f.init(0.0, -units, 0.0), delta_time);
     }
 
     pub fn setTargetPosition(self: *Camera, target: vector.Vec3f) void {
@@ -90,26 +108,42 @@ pub const Camera = struct {
 
     pub fn orthographicProjection(self: *Camera, scale_factor: u32, point: vector.Vec3f) vector.Vec2f {
         const scale_factor_f32 = utils.castU32xF32(scale_factor);
-        // shift point to positive space
         const x = (point.x() - self.min.x()) * scale_factor_f32;
         const y = (point.y() - self.min.y()) * scale_factor_f32;
         return vector.Vec2f.init(x, y);
     }
 
-    pub fn perspectiveProjection(self: *Camera, point: vector.Vec3f) ?vector.Vec2f {
+    pub const ProjectionResult = struct {
+        success: bool,
+        point: vector.Vec2f,
+    };
 
-        // transform the point by subtracting the camera position
+    pub fn perspectiveProjection(self: *Camera, point: vector.Vec3f) ProjectionResult {
         const translated_point = point.sub(self.position);
-
-        // cut off points that are behind the camera or too far away
         if (translated_point.z() < self.near_plane or translated_point.z() > self.far_plane) {
-            return null;
+            return ProjectionResult{ .success = false, .point = vector.Vec2f.init(0, 0) };
         }
-
-        //const x = (self.fov_factor * point.x()) / point.z();
-        //const y = (self.fov_factor * point.y()) / point.z();
         const x = (self.fov_factor * translated_point.x()) / translated_point.z();
         const y = (self.fov_factor * translated_point.y()) / translated_point.z();
-        return vector.Vec2f.init(x, y);
+        return ProjectionResult{ .success = true, .point = vector.Vec2f.init(x, y) };
+    }
+
+    fn resetTargetPosition(self: *Camera) void {
+        self.target_position = vector.Vec3f.init(0.0, 0.0, 0.0);
+    }
+
+    fn getForwardVector(self: *Camera) vector.Vec3f {
+        const yaw = self.orientation.x;
+        const pitch = self.orientation.y;
+        return vector.Vec3f.init(math.cos(yaw) * math.cos(pitch), math.sin(pitch), math.sin(yaw) * math.cos(pitch));
+    }
+
+    fn getRightVector(self: *Camera) vector.Vec3f {
+        const yaw = self.orientation.x;
+        return vector.Vec3f.init(math.sin(yaw - math.pi / 2.0), 0.0, math.cos(yaw - math.pi / 2.0));
+    }
+
+    fn getUpVector(_: *Camera) vector.Vec3f {
+        return vector.Vec3f.init(0.0, 1.0, 0.0);
     }
 };

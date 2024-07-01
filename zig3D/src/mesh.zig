9df47e36f12c0e686bc6cbc896ac2569b357af69
Axe <seamac@gmail.com>
2024-07-01 22:22:19 +0100

    Move LoadFile to Mesh struct

--- zig3D/src/mesh.zig
+++ zig3D/src/mesh.zig
@@ -69,89 +69,89 @@ pub const Mesh = struct {
             self.normals.items[i] = transform(normal, param);
         }
     }
-};
 
-pub fn LoadFromFile(path: []const u8, allocator: std.mem.Allocator) !Mesh {
-    var mesh = try Mesh.init(allocator);
-    var file = try std.fs.cwd().openFile(path, .{});
-    defer file.close();
+    pub fn LoadFromFile(path: []const u8, allocator: std.mem.Allocator) !Mesh {
+        var mesh = try Mesh.init(allocator);
+        var file = try std.fs.cwd().openFile(path, .{});
+        defer file.close();
 
-    var buffered = std.io.bufferedReader(file.reader());
-    var reader = buffered.reader();
+        var buffered = std.io.bufferedReader(file.reader());
+        var reader = buffered.reader();
 
-    var arr = std.ArrayList(u8).init(allocator);
-    defer arr.deinit();
+        var arr = std.ArrayList(u8).init(allocator);
+        defer arr.deinit();
 
-    while (true) {
-        reader.streamUntilDelimiter(arr.writer(), '\n', null) catch |err| switch (err) {
-            error.EndOfStream => break,
-            else => return err,
-        };
-        var lines = std.mem.splitAny(u8, arr.items, "\n"); // todo: fix this, terminator on windows is \r\n
-        if (std.mem.startsWith(u8, arr.items, "v ")) {
-            while (lines.next()) |line| {
-                if (!std.mem.eql(u8, line, " ")) {
-                    if (line.len < 4) {
-                        continue;
+        while (true) {
+            reader.streamUntilDelimiter(arr.writer(), '\n', null) catch |err| switch (err) {
+                error.EndOfStream => break,
+                else => return err,
+            };
+            var lines = std.mem.splitAny(u8, arr.items, "\n"); // todo: fix this, terminator on windows is \r\n
+            if (std.mem.startsWith(u8, arr.items, "v ")) {
+                while (lines.next()) |line| {
+                    if (!std.mem.eql(u8, line, " ")) {
+                        if (line.len < 4) {
+                            continue;
+                        }
+                        var line_components = std.mem.splitAny(u8, line, " ");
+                        _ = line_components.next(); // ignore the first element
+                        const x = try std.fmt.parseFloat(f32, line_components.next().?);
+                        const y = try std.fmt.parseFloat(f32, line_components.next().?);
+                        const z = try std.fmt.parseFloat(f32, line_components.next().?);
+                        try mesh.addVertex(x, -y, z);
                     }
-                    var line_components = std.mem.splitAny(u8, line, " ");
-                    _ = line_components.next(); // ignore the first element
-                    const x = try std.fmt.parseFloat(f32, line_components.next().?);
-                    const y = try std.fmt.parseFloat(f32, line_components.next().?);
-                    const z = try std.fmt.parseFloat(f32, line_components.next().?);
-                    try mesh.addVertex(x, -y, z);
+                }
+            } else if (std.mem.startsWith(u8, arr.items, "vn ")) {
+                while (lines.next()) |line| {
+                    if (!std.mem.eql(u8, line, " ")) {
+                        if (line.len < 5) {
+                            continue;
+                        }
+                        var line_components = std.mem.splitAny(u8, line, " ");
+                        _ = line_components.next(); // ignore the first element
+                        const x = try std.fmt.parseFloat(f32, line_components.next().?);
+                        const y = try std.fmt.parseFloat(f32, line_components.next().?);
+                        const z = try std.fmt.parseFloat(f32, line_components.next().?);
+                        try mesh.addNormal(x, y, z);
+                    }
+                }
+            } else if (std.mem.startsWith(u8, arr.items, "vt ")) {
+                while (lines.next()) |line| {
+                    if (!std.mem.eql(u8, line, " ")) {
+                        if (line.len < 5) {
+                            continue;
+                        }
+                        var line_components = std.mem.splitAny(u8, line, " ");
+                        _ = line_components.next(); // ignore the first element
+                        const u = try std.fmt.parseFloat(f32, line_components.next().?);
+                        const v = try std.fmt.parseFloat(f32, line_components.next().?);
+                        try mesh.addTextureCoord(u, v);
+                    }
+                }
+            } else if (std.mem.startsWith(u8, arr.items, "f ")) {
+                while (lines.next()) |line| {
+                    var parts = std.mem.splitAny(u8, line, " ");
+                    _ = parts.next(); // ignore the first element
+
+                    const part1 = parts.next() orelse return error.InvalidFormat;
+                    var line_components = std.mem.splitAny(u8, part1, "/");
+                    const idx_a = try std.fmt.parseUnsigned(u32, line_components.next() orelse return error.InvalidFormat, 10);
+
+                    const part2 = parts.next() orelse return error.InvalidFormat;
+                    line_components = std.mem.splitAny(u8, part2, "/");
+                    const idx_b = try std.fmt.parseUnsigned(u32, line_components.next() orelse return error.InvalidFormat, 10);
+
+                    const part3 = parts.next() orelse return error.InvalidFormat;
+                    line_components = std.mem.splitAny(u8, part3, "/");
+                    const idx_c = try std.fmt.parseUnsigned(u32, line_components.next() orelse return error.InvalidFormat, 10);
+
+                    try mesh.addFace(idx_a - 1, idx_b - 1, idx_c - 1);
                 }
             }
-        } else if (std.mem.startsWith(u8, arr.items, "vn ")) {
-            while (lines.next()) |line| {
-                if (!std.mem.eql(u8, line, " ")) {
-                    if (line.len < 5) {
-                        continue;
-                    }
-                    var line_components = std.mem.splitAny(u8, line, " ");
-                    _ = line_components.next(); // ignore the first element
-                    const x = try std.fmt.parseFloat(f32, line_components.next().?);
-                    const y = try std.fmt.parseFloat(f32, line_components.next().?);
-                    const z = try std.fmt.parseFloat(f32, line_components.next().?);
-                    try mesh.addNormal(x, y, z);
-                }
-            }
-        } else if (std.mem.startsWith(u8, arr.items, "vt ")) {
-            while (lines.next()) |line| {
-                if (!std.mem.eql(u8, line, " ")) {
-                    if (line.len < 5) {
-                        continue;
-                    }
-                    var line_components = std.mem.splitAny(u8, line, " ");
-                    _ = line_components.next(); // ignore the first element
-                    const u = try std.fmt.parseFloat(f32, line_components.next().?);
-                    const v = try std.fmt.parseFloat(f32, line_components.next().?);
-                    try mesh.addTextureCoord(u, v);
-                }
-            }
-        } else if (std.mem.startsWith(u8, arr.items, "f ")) {
-            while (lines.next()) |line| {
-                var parts = std.mem.splitAny(u8, line, " ");
-                _ = parts.next(); // ignore the first element
 
-                const part1 = parts.next() orelse return error.InvalidFormat;
-                var line_components = std.mem.splitAny(u8, part1, "/");
-                const idx_a = try std.fmt.parseUnsigned(u32, line_components.next() orelse return error.InvalidFormat, 10);
-
-                const part2 = parts.next() orelse return error.InvalidFormat;
-                line_components = std.mem.splitAny(u8, part2, "/");
-                const idx_b = try std.fmt.parseUnsigned(u32, line_components.next() orelse return error.InvalidFormat, 10);
-
-                const part3 = parts.next() orelse return error.InvalidFormat;
-                line_components = std.mem.splitAny(u8, part3, "/");
-                const idx_c = try std.fmt.parseUnsigned(u32, line_components.next() orelse return error.InvalidFormat, 10);
-
-                try mesh.addFace(idx_a - 1, idx_b - 1, idx_c - 1);
-            }
+            arr.clearRetainingCapacity();
         }
 
-        arr.clearRetainingCapacity();
+        return mesh;
     }
-
-    return mesh;
-}
+};

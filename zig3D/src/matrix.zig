27997ae43a7168a3a71e73953defc72a87977195
Axe <seamac@gmail.com>
2024-07-09 10:02:06 +0100

    matrix code placeholder

---
+++ zig3D/src/matrix.zig
@@ -0,0 +1,6 @@
+///! Matrix module
+///! This module contains the matrix struct and its implementation
+///! Supports basic matrix operations like addition, subtraction, multiplication, etc.
+///! Also supports matrix transformations like transpose, inverse, etc.
+/// Matrix 4x4
+pub const Mat4 = struct {};

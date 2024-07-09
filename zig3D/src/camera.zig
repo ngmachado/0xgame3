5a10f527aa07112438c866802ec56e53be7d997f
Axe <seamac@gmail.com>
2024-07-09 09:59:56 +0100

    add perspectiveProjectionMatrix

--- zig3D/src/camera.zig
+++ zig3D/src/camera.zig
@@ -1,19 +1,8 @@
-///! Camera module
-///!
-///! Config:
-///!     - position: Vec3f32 (camera position)
-///!     - screen_width: u32 (screen width)
-///!     - screen_height: u32 (screen height)
-///!     - fov_factor: f32 (field of view factor)
-///!     - near_plane: f32 (near plane)
-///!     - far_plane: f32 (far plane)
-///!     - speed: f32 (camera speed)
-///!
 const math = @import("std").math;
 const vector = @import("vector.zig");
 const Vec3f32 = vector.Vec3f32;
 const Vec2f32 = vector.Vec2f32;
-const ColorBuffer = @import("color_buffer.zig").ColorBuffer;
+const Vec4f32 = vector.Vec4f32;
 const std = @import("std");
 const utils = @import("utils.zig");
 
@@ -22,6 +11,7 @@ pub const Camera = struct {
     target_position: Vec3f32,
     screen_width: u32,
     screen_height: u32,
+    aspect_ratio: f32,
     fov: f32,
     near_plane: f32,
     far_plane: f32,
@@ -43,6 +33,7 @@ pub const Camera = struct {
             .target_position = Vec3f32.zero(),
             .screen_width = config.screen_width,
             .screen_height = config.screen_height,
+            .aspect_ratio = utils.castU32xF32(config.screen_width) / utils.castU32xF32(config.screen_height),
             .fov = config.fov_factor,
             .near_plane = config.near_plane,
             .far_plane = config.far_plane,
@@ -75,21 +66,47 @@ pub const Camera = struct {
         return self.position;
     }
 
-    pub fn orthographicProjection(self: *Camera, scale_factor: u32, point: Vec3f32) Vec2f32 {
-        const scale_factor_f32 = utils.castU32xF32(scale_factor);
-        const x = (point.x() - self.min.x()) * scale_factor_f32;
-        const y = (point.y() - self.min.y()) * scale_factor_f32;
+    /// returns the perspective projection matrix for a left-handed coordinate system
+    fn perspectiveProjectionMatrix(self: *Camera) [4][4]f32 {
+        const aspect_ratio = self.getScreenWidth() / self.getScreenHeight();
+        const fov_rad = math.degreesToRadians(self.fov);
+        const f = 1.0 / math.tan(fov_rad / 2.0);
+        const depth_range = self.far_plane - self.near_plane;
+
+        return [4][4]f32{
+            [_]f32{ f / aspect_ratio, 0, 0, 0 },
+            [_]f32{ 0, f, 0, 0 },
+            [_]f32{ 0, 0, (self.far_plane + self.near_plane) / depth_range, (2.0 * self.far_plane * self.near_plane) / depth_range },
+            [_]f32{ 0, 0, 1, 0 },
+        };
+    }
+
+    pub fn worldToView(self: *Camera, point: Vec3f32) Vec3f32 {
+        return point.sub(self.position);
+    }
+
+    pub fn viewToClip(self: *Camera, point: Vec3f32) Vec4f32 {
+        const proj_matrix = self.perspectiveProjectionMatrix();
+        return Vec4f32.init(point.x() * proj_matrix[0][0], point.y() * proj_matrix[1][1], point.z() * proj_matrix[2][2] + proj_matrix[2][3], point.z());
+    }
+
+    pub fn clipToNDC(_: *Camera, clip_coords: Vec4f32) Vec3f32 {
+        return Vec3f32.init(clip_coords.x() / clip_coords.w(), clip_coords.y() / clip_coords.w(), clip_coords.z() / clip_coords.w());
+    }
+
+    pub fn ndcToScreen(self: *Camera, ndc_coords: Vec3f32) Vec2f32 {
+        const x = (ndc_coords.x() + 1.0) * 0.5 * self.getScreenWidth();
+        const y = (1.0 - ndc_coords.y()) * 0.5 * self.getScreenHeight();
         return Vec2f32.init(x, y);
     }
 
-    pub fn perspectiveProjection(self: *Camera, point: Vec3f32) ?Vec2f32 {
-        const translated_point = point.sub(self.position);
-        if (translated_point.z() < self.near_plane or translated_point.z() > self.far_plane) {
+    pub fn project(self: *Camera, point: Vec3f32) ?Vec2f32 {
+        const view_coords = self.worldToView(point);
+        const clip_coords = self.viewToClip(view_coords);
+        const ndc_coords = self.clipToNDC(clip_coords);
+        if (ndc_coords.z() < 0.0 or ndc_coords.z() > 1.0) {
             return null;
         }
-
-        const x = (self.fov * translated_point.x()) / translated_point.z();
-        const y = (self.fov * translated_point.y()) / translated_point.z();
-        return Vec2f32.init(x, y);
+        return self.ndcToScreen(ndc_coords);
     }
 };
